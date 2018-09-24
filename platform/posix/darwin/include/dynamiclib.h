b634e6a443fbf31283cab2f3b1febe65e68af504
Paul Hollinsky <phollinsky@intrepidcs.com>
2018-09-24 18:16:08 -0400

    Point macOS version towards the right dylib
    This is super temporary
    
    Eventually we'll decide where the library will go

--- platform/posix/darwin/include/dynamiclib.h
+++ platform/posix/darwin/include/dynamiclib.h
@@ -1,6 +1,6 @@
 #ifndef __DYNAMICLIB_DARWIN_H_
 #define __DYNAMICLIB_DARWIN_H_
 
-#define icsneoDynamicLibraryLoad() dlopen("/home/paulywog/Code/icsneonext/build/libicsneoc.dylib", RTLD_LAZY)
+#define icsneoDynamicLibraryLoad() dlopen("/Users/paulywog/Code/icsneonext/build/libicsneoc.dylib", RTLD_LAZY)
 
 #endif
