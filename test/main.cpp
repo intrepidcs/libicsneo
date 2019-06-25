1dd68eb0d0acfee87631fb012409ef1a661c19ae
EricLiu2000 <ericxliu@gmail.com>
2019-06-25 17:47:26 -0400

    Test framework tentatively working on windows

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,23 @@
+#include "icsneo/api/eventmanager.h"
 #include "gtest/gtest.h"
 
-int main(int argc, char** argv)
-{
+using namespace icsneo;
+
+class EventManagerTest : public ::testing::Test {
+
+};
+
+TEST_F(EventManagerTest, GetLastErrorTest) {
+    EventManager::GetInstance().add(APIEvent(APIEvent::Type::OutputTruncated, APIEvent::Severity::Error));
+    EXPECT_EQ(EventManager::GetInstance().getLastError().getType(), APIEvent::Type::OutputTruncated);
+}
+
+TEST_F(EventManagerTest, CountTest) {
+    EventManager::GetInstance().add(APIEvent(APIEvent::Type::OutputTruncated, APIEvent::Severity::Error));
+    EXPECT_EQ(EventManager::GetInstance().count(), 1);
+}
+
+int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
