8f6ff8644815bb58006b2d9fdc72600d12d56299
Jeffrey Quesnelle <jeffq@intrepidcs.com>
2020-06-12 13:55:34 -0400

    Load pcap_sendqueue functions on Windows

--- platform/windows/internal/pcapdll.cpp
+++ platform/windows/internal/pcapdll.cpp
@@ -39,10 +39,10 @@ PCAPDLL::PCAPDLL()
 		stats = (PCAPSTATS)GetProcAddress(dll, "pcap_stats");
 		next_ex = (PCAPNEXTEX)GetProcAddress(dll, "pcap_next_ex");
 		sendpacket = (PCAPSENDPACKET)GetProcAddress(dll, "pcap_sendpacket");
-		// sendqueue_alloc = (PCAPSENDQUEUEALLOC)GetProcAddress(dll, "pcap_sendqueue_alloc");
-		// sendqueue_queue = (PCAPSENDQUEUEQUEUE)GetProcAddress(dll, "pcap_sendqueue_queue");
-		// sendqueue_destroy = (PCAPSENDQUEUEDESTROY)GetProcAddress(dll, "pcap_sendqueue_destroy");
-		// sendqueue_transmit = (PCAPSENDQUEUETRANSMIT)GetProcAddress(dll, "pcap_sendqueue_transmit");
+		sendqueue_alloc = (PCAPSENDQUEUEALLOC)GetProcAddress(dll, "pcap_sendqueue_alloc");
+		sendqueue_queue = (PCAPSENDQUEUEQUEUE)GetProcAddress(dll, "pcap_sendqueue_queue");
+		sendqueue_destroy = (PCAPSENDQUEUEDESTROY)GetProcAddress(dll, "pcap_sendqueue_destroy");
+		sendqueue_transmit = (PCAPSENDQUEUETRANSMIT)GetProcAddress(dll, "pcap_sendqueue_transmit");
 		datalink = (PCAPDATALINK)GetProcAddress(dll, "pcap_datalink");
 		createsrcstr = (PCAPCREATESRCSTR)GetProcAddress(dll, "pcap_createsrcstr");
 		setbuff = (PCAPSETBUFF)GetProcAddress(dll, "pcap_setbuff");
@@ -50,9 +50,9 @@ PCAPDLL::PCAPDLL()
 			freealldevs == NULL || close == NULL ||
 			stats == NULL || next_ex == NULL ||
 			sendpacket == NULL || 
-			// sendqueue_alloc == NULL ||
-			// sendqueue_queue == NULL || sendqueue_destroy == NULL ||
-			// sendqueue_transmit == NULL || 
+			sendqueue_alloc == NULL ||
+			sendqueue_queue == NULL || sendqueue_destroy == NULL ||
+			sendqueue_transmit == NULL || 
 			datalink == NULL ||
 			createsrcstr == NULL || setbuff == NULL) {
 			closeDLL();
