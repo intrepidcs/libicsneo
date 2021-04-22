4b12d3aa4ded20c568a05d20d52b4c6f9c8afd75
Paul Hollinsky <phollinsky@intrepidcs.com>
2021-04-22 13:29:56 -0400

    EventCallback: Convert indentation to tabs

--- include/icsneo/api/eventcallback.h
+++ include/icsneo/api/eventcallback.h
@@ -11,28 +11,28 @@ namespace icsneo {
 
 class EventCallback {
 public:
-    typedef std::function< void( std::shared_ptr<APIEvent> ) > fn_eventCallback;
+	typedef std::function< void( std::shared_ptr<APIEvent> ) > fn_eventCallback;
 
-    EventCallback(fn_eventCallback cb, std::shared_ptr<EventFilter> f) : callback(cb), filter(f) {}
-    EventCallback(fn_eventCallback cb, EventFilter f = EventFilter()) : callback(cb), filter(std::make_shared<EventFilter>(f)) {}
+	EventCallback(fn_eventCallback cb, std::shared_ptr<EventFilter> f) : callback(cb), filter(f) {}
+	EventCallback(fn_eventCallback cb, EventFilter f = EventFilter()) : callback(cb), filter(std::make_shared<EventFilter>(f)) {}
 
-    // Allow the filter to be placed first if the user wants (maybe in the case of a lambda)
+	// Allow the filter to be placed first if the user wants (maybe in the case of a lambda)
 	EventCallback(std::shared_ptr<EventFilter> f, fn_eventCallback cb) : callback(cb), filter(f) {}
 	EventCallback(EventFilter f, fn_eventCallback cb) : callback(cb), filter(std::make_shared<EventFilter>(f)) {}
 
-    virtual bool callIfMatch(const std::shared_ptr<APIEvent>& event) const {
+	virtual bool callIfMatch(const std::shared_ptr<APIEvent>& event) const {
 		bool ret = filter->match(*event);
 		if(ret)
 			callback(event);
 		return ret;
 	}
 
-    const EventFilter& getFilter() const { return *filter; }
+	const EventFilter& getFilter() const { return *filter; }
 	const fn_eventCallback& getCallback() const { return callback; }
 
 private:
-    fn_eventCallback callback;
-    std::shared_ptr<EventFilter> filter;
+	fn_eventCallback callback;
+	std::shared_ptr<EventFilter> filter;
 
 };
 
