80362e7f81a5db88d4cc10c9c6192acd902d6fde
Paul Hollinsky <phollinsky@intrepidcs.com>
2021-11-15 21:56:21 -0500

    Event Manager: Refactor

--- api/icsneocpp/eventmanager.cpp
+++ api/icsneocpp/eventmanager.cpp
@@ -8,6 +8,99 @@ EventManager& EventManager::GetInstance() {
 	return inst;
 }
 
+void EventManager::downgradeErrorsOnCurrentThread() {
+	if(destructing)
+		return;
+	std::lock_guard<std::mutex> lk(downgradedThreadsMutex);
+	auto i = downgradedThreads.find(std::this_thread::get_id());
+	if(i != downgradedThreads.end()) {
+		i->second = true;
+	} else {
+		downgradedThreads.insert({std::this_thread::get_id(), true});
+	}
+}
+
+void EventManager::cancelErrorDowngradingOnCurrentThread() {
+	if(destructing)
+		return;
+	std::lock_guard<std::mutex> lk(downgradedThreadsMutex);
+	auto i = downgradedThreads.find(std::this_thread::get_id());
+	if(i != downgradedThreads.end()) {
+		i->second = false;
+	}
+}
+
+void EventManager::add(APIEvent event) {
+	if(destructing)
+		return;
+	if(event.getSeverity() == APIEvent::Severity::Error) {
+		// if the error was added on a thread that downgrades errors (non-user thread)
+		std::lock_guard<std::mutex> lk(downgradedThreadsMutex);
+		auto i = downgradedThreads.find(std::this_thread::get_id());
+		if(i != downgradedThreads.end() && i->second) {
+			event.downgradeFromError();
+			{
+				std::lock_guard<std::mutex> eventsLock(eventsMutex);
+				addEventInternal(event);
+			} // free the lock so that callbacks may modify events
+			runCallbacks(event);
+		} else {
+			std::lock_guard<std::mutex> errorsLock(errorsMutex);
+			lastUserErrors[std::this_thread::get_id()] = event;
+		}
+	} else {
+		{
+			std::lock_guard<std::mutex> eventsLock(eventsMutex);
+			addEventInternal(event);
+		} // free the lock so that callbacks may modify events
+		runCallbacks(event);
+	}
+}
+
+void EventManager::addEventInternal(APIEvent event) {
+	// Ensure the event list is at most exactly full (size of eventLimit - 1, leaving room for a potential APIEvent::TooManyEvents)
+	// Removes any events of type TooManyEvents from the end before checking to avoid duplicates.
+	enforceLimit();
+
+	// We are exactly full, either because the list was truncated or because we were simply full before
+	if(events.size() == eventLimit - 1) {
+		// If the event is worth adding
+		if(event.getType() != APIEvent::Type::TooManyEvents) {
+			discardOldest(1);
+			events.push_back(event);
+		}
+
+		events.push_back(APIEvent(APIEvent::Type::TooManyEvents, APIEvent::Severity::EventWarning));
+	} else {
+		if (event.getType() != APIEvent::Type::TooManyEvents)
+			events.push_back(event);
+	}
+}
+
+void EventManager::runCallbacks(APIEvent event) {
+	std::lock_guard<std::mutex> lk(callbacksMutex);
+	for(auto& i : callbacks)
+		i.second.callIfMatch(std::make_shared<APIEvent>(event));
+}
+
+void EventManager::setEventLimit(size_t newLimit) {
+	std::lock_guard<std::mutex> eventLimitLock(eventLimitMutex);
+	
+	if(newLimit == eventLimit)
+		return;
+	
+	if(newLimit < 10) {
+		add(APIEvent::Type::ParameterOutOfRange, APIEvent::Severity::Error);
+		return;
+	}
+
+	eventLimit = newLimit;
+	
+	std::lock_guard<std::mutex> eventsLock(eventsMutex);
+	if(enforceLimit())
+		addEventInternal(APIEvent(APIEvent::Type::TooManyEvents, APIEvent::Severity::EventWarning));
+}
+
 void EventManager::ResetInstance() {
 	std::lock_guard<std::mutex> eventsLock(eventsMutex);
 	std::lock_guard<std::mutex> errorsLock(errorsMutex);
@@ -21,7 +114,7 @@ void EventManager::ResetInstance() {
 	lastUserErrors.clear();
 	downgradedThreads.clear();
 	callbacks.clear();
-	
+
 	callbackID = 0;
 	eventLimit = 10000;
 }
@@ -55,7 +148,7 @@ bool EventManager::isDowngradingErrorsOnCurrentThread() const {
 
 void EventManager::get(std::vector<APIEvent>& eventOutput, size_t max, EventFilter filter) {
 	std::lock_guard<std::mutex> lk(eventsMutex);
-	
+
 	if(max == 0) // A limit of 0 indicates no limit
 		max = (size_t)-1;
 
@@ -98,7 +191,7 @@ void EventManager::discard(EventFilter filter) {
 	});
 }
 
-size_t EventManager::count_internal(EventFilter filter) const {
+size_t EventManager::countInternal(EventFilter filter) const {
 	size_t ret = 0;
 	for(auto& event : events)
 		if(filter.match(event))
@@ -117,7 +210,7 @@ bool EventManager::enforceLimit() {
 	while(it != events.rend() && filter.match(*it)) {
 		it = decltype(it){events.erase( std::next(it).base() )};
 	}
-	
+
 	// We are not overflowing
 	if(events.size() < eventLimit)
 		return false;
@@ -137,6 +230,6 @@ void EventManager::discardOldest(size_t count) {
 	while(it != events.end()) {
 		it = events.erase(it);
 		if(--count == 0)
-			break;				
+			break;
 	}
 }
