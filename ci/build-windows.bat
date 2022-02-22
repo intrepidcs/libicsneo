416996b31db657bd2a1365895b34dc641bb6449b
Paul Hollinsky <phollinsky@intrepidcs.com>
2022-02-21 21:23:58 -0500

    CI: Warnings are errors

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -6,6 +6,7 @@ mkdir build
 
 REM build
 cd build
+set CXXFLAGS=/WX
 cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLIBICSNEO_BUILD_TESTS=ON ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake --build .
