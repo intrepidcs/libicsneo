733312628c0874ba948c69caa124cb0388ccf8b9
Paul Hollinsky <phollinsky@intrepidcs.com>
2022-02-22 10:37:37 -0500

    CI: Also set warnings as errors in C code

--- ci/build-windows.bat
+++ ci/build-windows.bat
@@ -4,6 +4,7 @@ mkdir build
 
 REM build
 cd build
+set CFLAGS=/WX
 set CXXFLAGS=/WX
 cmake -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLIBICSNEO_BUILD_TESTS=ON ..
 if %errorlevel% neq 0 exit /b %errorlevel%
