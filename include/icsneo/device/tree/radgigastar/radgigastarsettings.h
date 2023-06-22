e0117f554209c2120d09fc4284618292ad36ff69
Francesco Valla <francesco.valla@mta.it>
2023-06-22 11:38:28 +0200

    Device: add LIN settings getter for devices with LIN

--- include/icsneo/device/tree/radgigastar/radgigastarsettings.h
+++ include/icsneo/device/tree/radgigastar/radgigastarsettings.h
@@ -149,6 +149,18 @@ public:
 		};
 	}
 
+	const LIN_SETTINGS* getLINSettingsFor(Network net) const override {
+		auto cfg = getStructurePointer<radgigastar_settings_t>();
+		if(cfg == nullptr)
+			return nullptr;
+		switch(net.getNetID()) {
+			case Network::NetID::LIN:
+				return &(cfg->lin1);
+			default:
+				return nullptr;
+		}
+	}
+
 protected:
 	ICSNEO_UNALIGNED(const uint64_t*) getTerminationEnables() const override {
 		auto cfg = getStructurePointer<radgigastar_settings_t>();
