19df557b4a835734528ac1c5a64e6e7df071e998
Nicholas Zamora <nzamora@intrepidcs.com>
2025-07-09 20:48:22 +0000

    DeviceAppVersion: Add operator==

--- include/icsneo/device/deviceversion.h
+++ include/icsneo/device/deviceversion.h
@@ -13,7 +13,12 @@ struct DeviceAppVersion {
 	uint8_t major = 0;
 	uint8_t minor = 0;
 
+	DeviceAppVersion() = default;
+	DeviceAppVersion(const uint8_t major, const uint8_t minor)
+		: major(major), minor(minor) {}
+
 	bool operator!=(const DeviceAppVersion& rhs) const { return major != rhs.major || minor != rhs.minor; }
+	bool operator==(const DeviceAppVersion& rhs) const { return major == rhs.major && minor == rhs.minor; }
 };
 
 } // namespace icsneo
