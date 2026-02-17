224e8408417977fe1d1a295c381e7eb39786bd27
Kyle Schwarz <kschwarz@intrepidcs.com>
2026-02-17 17:16:49 -0500

    Device: Increase DiskFormatProgress timeout

--- device/device.cpp
+++ device/device.cpp
@@ -3817,7 +3817,7 @@ bool Device::formatDisk(const DiskDetails& config, const DiskFormatProgress& han
 				return com->sendCommand(ExtendedCommand::DiskFormatProgress, {}); 
 			},
 			std::make_shared<ExtendedResponseFilter>(ExtendedCommand::DiskFormatProgress),
-			std::chrono::milliseconds(200)
+			std::chrono::milliseconds(1000)
 		);
 
 		if(!response) {
